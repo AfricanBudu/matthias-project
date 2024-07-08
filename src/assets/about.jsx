da25f4b70cf9ba0f4c483ff43c2153e3fe88f291
AfricanBudu <abdullahagboola202@gmail.com>
2024-07-08 15:39:24 +0100

    No worry much

--- src/assets/about.jsx
+++ src/assets/about.jsx
@@ -1,7 +1,7 @@
 
 const about = () => {
   return (
-    <div>
+    <div className=" overflow-x-hidden relative">
         <div className="overflow-hidden mt-10">
             <div className="relative h-56 flex items-end justify-start overflow-hidden font-Semibold_Grotesk">
                 <div className="absolute text-[15.625rem] text-[#5353533B] tracking-tight leading-none -left-6 bottom-0">
@@ -14,12 +14,41 @@ const about = () => {
                 </div>
             </div>
         </div>
-        <div className="container px-44 flex">
+
+
+        <div className="container pl-44 pr-20 flex justify-between">
             <div>
                 <img src="/images/matthias-about-image.png" alt="" />
             </div>
-            <div></div>
+            <div className="pt-3">
+                <p className="text-[#D6B89F] text-[2.8125rem] leading-[2.9375rem] font-Medium_Grotesk">
+                “To be completely <br/>engaged with a material <br/>is a way 
+                to cheat time, to <br/>slow it down. My work <br/>ethic and 
+                construction <br/>approaches are rooted <br/>in this discipline 
+                of <br/>concentrated <br/>engagement.”
+                </p>
+            </div>
         </div>
+
+        <div className="matthias-background w-full pt-40 pb-5">
+            <div className="absolute bg-gradient-to-r w-[15.875rem] h-[10.25rem] blur-[100px] top-[59rem] -right-20 rounded-[100%]" />
+            <div className="px-[3.625rem] text-[1.8rem] font-General_Sans font-medium">
+                <p className="leading-tight text-[#D9DBDC]">
+                    Pliessing is a graduate of Rhode Island School of Design 
+                    and the University of Wisconsin (Madison). His interest 
+                    in woodworking developed after he decided to build a boat and
+                    <br/>
+                    <br/>
+                    was inspired to apply those techniques, particularly 
+                    steam bending of wood strips <br/>around a mold, to furniture making.
+                </p>
+                <p className="text-[#FFEFCC] text-[1.5rem]">continue</p>
+            </div>
+            <div className="flex justify-center items-center px-[3rem] my-10 mt-20">
+                <img src="/images/matthias-background-mage.png" alt="" />
+            </div>
+        </div>
+
     </div>
   )
 }
